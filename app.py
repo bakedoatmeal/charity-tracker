9b14a370742c1a6714ba1d9548792e94db827a8a
Jane Cui <jane.anqi.cui@gmail.com>
2021-11-10 10:36:02 -0500

    added charity details

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ donations = db.donations
 
 app = Flask(__name__)
 
+charities = [
+    { 'name': 'SPCA', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'rating': 3, 'projects': [{}, {}], '_id': 1}, 
+    { 'name': 'Moisson Montreal', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'rating': 4, 'projects': [{}, {}], '_id': 2},
+    { 'name': 'World Wildlife Fund', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'rating': 5, 'projects': [{}, {}], '_id': 3}
+]
+
 @app.route('/')
 def index():
     """Return homepage"""
-    msg = "hello!"
-    return render_template('users_index.html', users=users.find())
-
-@app.route('/users')
-def users_index():
-    return render_template('users_index.html', users = users.find())
+    return render_template('users_index.html', users=users.find(), charities=charities)
 
 @app.route('/users/new')
 def users_new():
@@ -39,7 +40,7 @@ def users_show(user_id):
     """Show a single user's information."""
     user = users.find_one({'_id': ObjectId(user_id)})
     user_donations = donations.find({'user_id': ObjectId(user_id)})
-    return render_template('users_show.html', user = user, donations=user_donations)
+    return render_template('users_show.html', user = user, donations=user_donations, charities=charities)
 
 #TODO: Add update route for Users
 #TODO: Also, can make sub templates for html files
@@ -61,6 +62,15 @@ def donations_new():
     donations.insert_one(donation)
     # return redirect(url_for('users_show', user_id=request.form.get('user_id')))
     return redirect(url_for('users_show', user_id=request.form.get('user_id')))
+
+@app.route('/users/donations/<donation_id>', methods=['POST'])
+def donations_delete(donation_id):
+    donations.delete_one({'_id': ObjectId(donation_id)})
+    return redirect(url_for('users_show', user_id=request.form.get('user_id')))
+
+@app.route('/charities/<charity_id>')
+def charities_show(charity_id):
+    return render_template('charities_show.html', charity = charities[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
