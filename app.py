1df275744792ec89ce8585f718f63b25a75b5baa
Jane Cui <jane.anqi.cui@gmail.com>
2021-11-17 13:02:20 -0500

    add Procfile

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, url_for
 from  bson.objectid import ObjectId
+from dotenv import load_dotenv
+import os
+import pymongo
+import certifi
 
-client = MongoClient()
+ca = certifi.where()
+load_dotenv()
+DATABASE_URL = f'mongodb+srv://janecui:{os.environ.get("password")}@playlistr.k3tyy.mongodb.net/Playlister?retryWrites=true&w=majority'
+client = MongoClient(DATABASE_URL, tlsCAFile=ca)
 db = client.Donations
 users = db.users
 donations = db.donations
